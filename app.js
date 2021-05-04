975c64310ae1bfa5e8b33355a6efc765e5b6ac67
Anthony Barberis <anthonybarberis@outlook.com>
2021-05-04 11:37:15 -0700

    Created Creature class

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
-
-    // Create Dino Constructor
-
-
-    // Create Dino Objects
-
-
-    // Create Human Object
-
-    // Use IIFE to get human data from form
-
-
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
-
-    // Remove form from screen
-
-
-// On button click, prepare and display infographic
+class Creature {
+    constructor(species, weight, height, diet, where, when, fact) {
+        this.species = species;
+        this.weight = weight;
+        this.height = height;
+        this.diet = diet;
+        this.where = where;
+        this.when = when;
+        this.fact = fact;
+    }
+    compareCreatures(comparison, creature) {
+        switch (comparison) {
+            case 'weight':
+                break;
+            case 'height':
+                break;
+            case 'diet':
+                break
+        }
+    }
+    randomFact(rand) {
+        switch (rand) {
+            case 1: return this.where;
+            case 2: return this.when;
+            case 3: return this.fact;
+        }
+    }
+}
